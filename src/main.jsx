3ecbc0f1554ef0adb08d3a5355c5536c09a1452f
michael-duke <michael.21duke@gmail.com>
2023-01-09 00:06:01 +0300

    Import tailwind.css in index.css - Use a Material tailwind component in Example.jsx - Extend the App.jsx to use the Example.jsx component - Fix ESLint and Stylelint linter errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@material-tailwind/react';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <ThemeProvider>
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>,
+);
