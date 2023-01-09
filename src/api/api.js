157d57005de552db045fc07c97c117aac949324d
michael-duke <michael.21duke@gmail.com>
2023-01-09 22:33:13 +0300

    Implement api endpoint methods for login and register

---
+++ src/api/api.js
@@ -0,0 +1,70 @@
+const baseURL = 'http://localhost:3000/api/v1';
+
+const setAuthToken = ({ headers }) => {
+  localStorage.setItem('token', headers.get('Authorization'));
+};
+
+const loginOptions = (user) => ({
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(user),
+});
+
+const registerOptions = (user) => ({
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  // headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
+  body: JSON.stringify(user),
+});
+
+const carBookingOptions = (booking) => ({
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(booking),
+});
+
+const logoutOptions = () => ({
+  method: 'DELETE',
+  headers: { Authorization: localStorage.getItem('token') },
+});
+
+const api = {
+  register: async (user) => {
+    const response = await fetch(`${baseURL}/register`, {
+      ...registerOptions({ user }),
+    });
+
+    setAuthToken(response);
+
+    const data = await response.json();
+    return data;
+  },
+  login: async (user) => {
+    const response = await fetch(`${baseURL}/login`, {
+      ...loginOptions({ user }),
+    });
+
+    setAuthToken(response);
+
+    const data = await response.json();
+    return data;
+  },
+  logout: async () => {
+    const response = await fetch(`${baseURL}/logout`, {
+      ...logoutOptions(),
+    });
+
+    const data = await response.json();
+    return data;
+  },
+  reserveCar: async (id, booking) => {
+    const response = await fetch(`${baseURL}/users/${id}/reservations`, {
+      ...carBookingOptions({ booking }),
+    });
+
+    const data = await response.json();
+    return data;
+  },
+};
+
+export default api;
