20a87d37227ed47f6c3195677d8877ba409e4f38
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-10 20:09:24 +0300

    Add init booking page

---
+++ src/pages/BookingPage.jsx
@@ -0,0 +1,5 @@
+import Booking from '../components/Booking';
+
+const BookingPage = () => <Booking />;
+
+export default BookingPage;
--- src/pages/bookingPage/bookingPage.jsx
+++
@@ -1,7 +0,0 @@
-import Booking from '../../components/booking';
-
-const BookingPage = () => (
-  <Booking />
-);
-
-export default BookingPage;
