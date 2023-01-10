db8ce6451c9d18c0571c49917ebc1605a8fba9e1
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-01-10 13:00:01 +0500

    Add Home page router in the Route component.

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../pages/Home';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Home from "../pages/Home";
+import { getCars } from "../redux/Home/home";
 
-const AppRouter = () => (
-  <div className="flex">
-    <NavBar />
-    <div className="p-7 flex-1 h-screen">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/booking"
-          element={<h1>Booking component will be here</h1>}
-        />
-        <Route
-          path="/reservation"
-          element={<h1>Reservation component will be here</h1>}
-        />
-        <Route path="/login" element={<h1>Login component will be here</h1>} />
-      </Routes>
+const AppRouter = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCars());
+  }, []);
+
+  return (
+    <div className="flex">
+      <NavBar />
+      <div className="sm:p-2 md:p-3 flex-1 h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/booking"
+            element={<h1>Booking component will be here</h1>}
+          />
+          <Route
+            path="/reservation"
+            element={<h1>Reservation component will be here</h1>}
+          />
+          <Route
+            path="/login"
+            element={<h1>Login component will be here</h1>}
+          />
+        </Routes>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default AppRouter;
