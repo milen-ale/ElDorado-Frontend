460b920cace0171088ffc773238474768724d2a0
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-10 20:08:41 +0300

    Update replace date picker with react date picker

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -1,27 +1,26 @@
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { TextField } from '@mui/material';
-import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import dayjs from 'dayjs';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { format } from 'date-fns';
 import { bookCar } from '../redux/Reservations/reservationsSlice';
 import api from '../api/api';
 
 const Booking = () => {
+  const [pickupDate, setPickupDate] = useState(null);
+  const [returnDate, setReturnDate] = useState(null);
+  if (pickupDate) { console.log(format(pickupDate, 'yyyy-MM-dd')); }
   const user = {
     email: 'abc@gmail.com',
     password: '123456',
   };
   api.login(user);
   const dispatch = useDispatch();
-  const [selectedPickupDate, setPickupDate] = useState(null);
-  const [selectedReturnDate, setReturnDate] = useState(null);
   const handleSubmit = (e) => {
     e.preventDefault();
     const reservation = {
-      pickup_date: selectedPickupDate.format('YYYY-MM-DD'),
-      return_date: selectedReturnDate.format('YYYY-MM-DD'),
+      pickup_date: format(pickupDate, 'yyyy-MM-dd'),
+      return_date: format(returnDate, 'yyyy-MM-dd'),
       car_id: 19,
     };
     const reservationObject = {
@@ -34,26 +33,21 @@ const Booking = () => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <MobileDatePicker
-            label="Pickup Date"
-            renderInput={(params) => <TextField {...params} />}
-            value={selectedPickupDate}
-            minDate={dayjs(`${new Date().toLocaleString()}`)}
-            onChange={(newValue) => {
-              setPickupDate(newValue);
-            }}
-          />
-          <MobileDatePicker
-            label="return Date"
-            renderInput={(params) => <TextField {...params} />}
-            value={selectedReturnDate}
-            minDate={dayjs(`${new Date().toLocaleString()}`)}
-            onChange={(newValue) => {
-              setReturnDate(newValue);
-            }}
-          />
-        </LocalizationProvider>
+        <DatePicker
+          placeholderText="Pickup Date"
+          className="mb-5"
+          selected={pickupDate}
+          dateFormat="yyyy/MM/dd"
+          minDate={new Date()}
+          onChange={(date) => { setPickupDate(date); }}
+        />
+        <DatePicker
+          placeholderText="Return Date"
+          selected={returnDate}
+          dateFormat="yyyy/MM/dd"
+          minDate={new Date()}
+          onChange={(date) => { setReturnDate(date); }}
+        />
         <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l" type="submit">Submit</button>
       </form>
     </>
