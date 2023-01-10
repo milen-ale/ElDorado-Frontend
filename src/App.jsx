e5ade14e7906ff345a009ce57cf8f72aa62856e7
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-11 00:32:24 +0300

    Update remove redundant imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import AppRouter from './routers';
-import './App.css';
-import BookingPage from './pages/BookingPage';
 import 'tw-elements';
 
 const App = () => (
   <>
-    <BookingPage />
     <AppRouter />
   </>
 );
