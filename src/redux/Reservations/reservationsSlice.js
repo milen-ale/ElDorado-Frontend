3ec441622f2f7481aa627541ab603cc702722397
milen-ale <milenalebel2@gmail.com>
2023-01-11 13:26:52 +0300

    add delete and get reservations thunk

--- src/redux/Reservations/reservationsSlice.js
+++ src/redux/Reservations/reservationsSlice.js
@@ -3,22 +3,33 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../../api/api';
 
 // Actions
-const RESERVATIONS = 'RESERVATIONS';
-
+const RESERVE_CAR = 'RESERVE_CAR';
+const GET_RESERVATIONS = 'GET_RESERVATIONS';
+const DELETE_RESERVATION = 'DELETE_RESERVATION';
 const initialState = {
-
   reservations: [],
   status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
   message: '',
   error: null,
-
 };
-
 // Thunks
-
-export const bookCar = createAsyncThunk(RESERVATIONS, async ({ user_id: id, reservation }) => {
+export const bookCar = createAsyncThunk(RESERVE_CAR, async ({ user_id: id, reservation }) => {
   try {
     return await api.reserveCar(id, reservation);
+  } catch (error) {
+    return error.message;
+  }
+});
+export const getReservations = createAsyncThunk(GET_RESERVATIONS, async (userId) => {
+  try {
+    return await api.fetchReservations(userId);
+  } catch (error) {
+    return error.message;
+  }
+});
+export const deleteReservation = createAsyncThunk(DELETE_RESERVATION, async ({ userId, carId }) => {
+  try {
+    return await api.deleteReservation(userId, carId);
   } catch (error) {
     return error.message;
   }
@@ -43,6 +54,37 @@ const reservationsSlice = createSlice({
         status: 'succeeded',
       }))
       .addCase(bookCar.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }))
+      .addCase(getReservations.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(getReservations.fulfilled, (state, action) => ({
+        ...state,
+        reservations: action.payload,
+        message: action.payload.message,
+        status: 'succeeded',
+      }))
+      .addCase(getReservations.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+        error: action.error.message,
+      }))
+      .addCase(deleteReservation.pending, (state) => ({
+        ...state,
+        status: 'loading',
+      }))
+      .addCase(deleteReservation.fulfilled, (state, action) => ({
+        ...state,
+        reservations: [...state.reservations
+          .filter((reservation) => reservation.id === action.payload.data.id)],
+        message: action.payload.message,
+        status: 'succeeded',
+      }))
+      .addCase(deleteReservation.rejected, (state, action) => ({
         ...state,
         status: 'failed',
         error: action.error.message,
