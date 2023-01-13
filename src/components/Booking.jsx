79348caf30069425b27d8fc472628434ce8b1609
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-13 12:04:21 +0300

    Remove booking date-picker import

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { DatePicker } from 'antd';
-import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 import * as dayjs from 'dayjs';
 import {
