6dcc02ae80de2428e5fcaef9e22c63a62fc019d6
michael-duke <michael.21duke@gmail.com>
2023-01-13 13:39:38 +0300

    Imporve CarDetails and redirect to Booking form with the selected car

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -1,42 +1,59 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { car, getCar } from '../redux/Home/home';
 
-const CarDetails = () => {
+const CarDetails = ({ open }) => {
   const { id } = useParams();
   const carDetails = useSelector(car);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleReserve = () => {
+    navigate('/booking');
+  };
+
   useEffect(() => {
     dispatch(getCar(id));
   }, []);
   return (
-    <div className="flex justify-evenly  items-center">
-      <div>
+    <>
+      <div className="mt-10 flex items-center smax:flex-col gap-3">
         <img
-          className="w-[1000px] rounded-md "
+          className="w-[1000px] min-w-[270px] rounded-md self-stretch object-cover"
           src={carDetails.image}
           alt={carDetails.name}
         />
-      </div>
-      <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">{carDetails.name}</h1>
-        <p className="text-sm w-72">
-          {' '}
-          {carDetails.description}
-        </p>
-        <div className=" bg-gray-400 text-center">
-          <p className="font-bold">{carDetails.model}</p>
-        </div>
-        <div className=" bg-blue-400 text-center">
-          <p className="font-bold">
+        <div className="flex flex-col gap-4 self-stretch">
+          <h1 className="text-2xl font-semibold self-end justify-self-start font-osans">
+            {carDetails.name}
+          </h1>
+
+          <p className="font-bold bg-gray-400 text-center px-7">
+            {carDetails.model}
+          </p>
+
+          <p className=" bg-white text-black text-center font-bold px-7">
             $
             {carDetails.daily_price}
+            {' '}
+            /day
           </p>
+          <p className={`text-base max-w-[800px] ${open && 'text-xs'}`}>{carDetails.description}</p>
+          <button onClick={handleReserve} type="button">
+            <span className="text-black font-bold bg-amber-500 border px-4 hover:bg-white hover:border-amber-500 hover:text-amber-500 py-3 rounded-sm">
+              Reserve
+            </span>
+          </button>
         </div>
       </div>
-    </div>
+    </>
   );
+};
+
+CarDetails.propTypes = {
+  open: PropTypes.bool.isRequired,
 };
 
 export default CarDetails;
