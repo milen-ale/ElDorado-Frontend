873a398c63959f5999c84a520a8f47e8f00e7f83
milen-ale <milenalebel2@gmail.com>
2023-01-14 21:09:25 +0300

    add addCarPage.jsx to the page file

--- src/components/DeleteCar.jsx
+++ src/components/DeleteCar.jsx
@@ -4,7 +4,7 @@ import {
   CardHeader,
   CardBody,
   CardFooter,
-  Typography
+  Typography,
 } from '@material-tailwind/react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
@@ -12,7 +12,7 @@ import {
   allStatus,
   toggleAvailability,
   getOwnerCars,
-  setMessageEmpty
+  setMessageEmpty,
 } from '../redux/Home/home';
 import { useAuthUser } from '../redux/Auth/useAuthUser';
 import Loader from './Loader';
@@ -26,7 +26,7 @@ const DeleteCar = () => {
 
   const handleDeleteCar = (status, carId) => {
     const car = {
-      available: status
+      available: status,
     };
 
     setTimeout(() => {
@@ -41,23 +41,23 @@ const DeleteCar = () => {
   return (
     <>
       <CardHeader
-        variant='gradient'
-        className='mb-4 grid h-28 place-items-center text-white bg-black/50 backdrop-blur-md'
+        variant="gradient"
+        className="mb-4 grid h-28 place-items-center text-white bg-black/50 backdrop-blur-md"
       >
         <Typography
-          variant='h3'
-          color='white'
-          className='font-osans uppercase tracking-widest font-light'
+          variant="h3"
+          color="white"
+          className="font-osans uppercase tracking-widest font-light"
         >
           Delete a Car
         </Typography>
       </CardHeader>
       {status === 'loading' ? (
-        <div className='flex items-center justify-center h-96 w-[75vw]'>
+        <div className="flex items-center justify-center h-96 w-[75vw]">
           <Loader />
         </div>
       ) : (
-        <div className='Car-Grid grid gap-6'>
+        <div className="Car-Grid grid gap-6">
           {userCars.map(
             ({
               id: carId,
@@ -65,26 +65,29 @@ const DeleteCar = () => {
               model,
               image,
               daily_price: price,
-              available
+              available,
             }) => (
-              <Card key={carId} className='cursor-pointer my-5'>
-                <CardHeader color='amber' className='relative h-56 mx-0.5'>
+              <Card key={carId} className="cursor-pointer my-5">
+                <CardHeader color="amber" className="relative h-56 mx-0.5">
                   <img
                     src={image}
-                    alt='img-blur-shadow'
-                    className='h-full w-full object-cover'
+                    alt="img-blur-shadow"
+                    className="h-full w-full object-cover"
                   />
                 </CardHeader>
-                <CardBody className='px-2 text-center'>
-                  <Typography variant='h5' className='mb-2 whitespace-pre-wrap'>
+                <CardBody className="px-2 text-center">
+                  <Typography variant="h5" className="mb-2 whitespace-pre-wrap">
                     {name}
                   </Typography>
                 </CardBody>
                 <CardFooter
                   divider
-                  className='flex items-center justify-between py-3'
+                  className="flex items-center justify-between py-3"
                 >
-                  <Typography variant='small'>${price}</Typography>
+                  <Typography variant="small">
+                    $
+                    {price}
+                  </Typography>
                   {/* <Button
                     color="amber"
                     className="text-xs"
@@ -99,15 +102,15 @@ const DeleteCar = () => {
                     handleRemove={handleDeleteCar}
                   />
                   <Typography
-                    variant='small'
-                    color='gray'
-                    className='flex gap-1'
+                    variant="small"
+                    color="gray"
+                    className="flex gap-1"
                   >
                     {model}
                   </Typography>
                 </CardFooter>
               </Card>
-            )
+            ),
           )}
         </div>
       )}
