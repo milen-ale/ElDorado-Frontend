e3d982e8a71ffb859aaf8527ec2b92855a495ed3
milen-ale <milenalebel2@gmail.com>
2023-01-16 13:26:09 +0300

    add home page testes

---
+++ src/__tests__/homePage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import store from '../redux/configureStore';
+import '@testing-library/jest-dom/';
+import Home from '../pages/Home';
+
+describe('Home Page', () => {
+  test('Should render the proper Home Page', () => {
+    const homePage = render(
+      <Provider store={store}>
+        <Router>
+          <Home />
+        </Router>
+      </Provider>,
+    );
+    expect(homePage).toMatchSnapshot();
+  });
+});
