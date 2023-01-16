8c37772542230e0fee73de5e6e255a87cae3f57b
AbelG101 <abelgebeyehu101@gmail.com>
2023-01-16 13:51:59 +0300

    Add snapshot test for Login component

---
+++ src/__tests__/Login.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import store from '../redux/configureStore';
+import '@testing-library/jest-dom/';
+import Login from '../components/Login';
+
+describe('Login component', () => {
+  test('Should render the proper Login component', () => {
+    const login = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Login />
+          </Router>
+        </Provider>
+        ,
+      </React.StrictMode>,
+    );
+    expect(login).toMatchSnapshot();
+  });
+});
